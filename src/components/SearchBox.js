ac51f0e97987518b89309243b2eff189088509c6
_cpoptile <poptilec@msoe.edu>
2021-03-15 15:12:42 -0500

    Implement new fetch of API -still testing

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import './SearchBox.css';
 import Places from './Places';
+import Datepicker from 'react-datepicker';
 
 function SearchBox() { 
   const [places, setPlaces] = useState([])
+  const [fromLocation, setFromLocation] = useState("")
+  const [toLocation, setToLocation] = useState("")
   const [query, setQuery] = useState("")
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
   const [showPlaces,setShowPlaces] = useState(false)
+
+  const handleStartDate = date => {
+    setStartDate(date)
+  }
+
+  const handleEndDate = date => {
+    setEndDate(date)
+  }
 
   function handleSubmit(e) {
       e.preventDefault()
@@ -29,6 +42,24 @@ function SearchBox() {
       setShowPlaces(true)
   }
 
+  function handleSearch(fromLocation, toLocation, startDate){
+    async function findFlights() {
+      const url = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${fromLocation}/${toLocation}/${startDate}?` +
+      new URLSearchParams({ inboundpartialdate: endDate });
+      const reqOptions = {
+        method: 'GET',
+        headers: {
+            "x-rapidapi-key": "42a5ad4309msh0f7539296c11895p172628jsn1bac294685a3",
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        },
+      };
+      let response = await fetch(url, reqOptions)
+      response = await response.json();
+    }
+
+    }
+
+
   // function doAjaxRequest(){
   //   $.ajax({
   //     url: "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/"
@@ -41,11 +72,22 @@ function SearchBox() {
           <form onSubmit={handleSubmit}>
                 <label htmlFor="queryInput">State or Country:</label>
                 <input id="queryInput" value={query} onChange={e => setQuery(e.target.value)} required/>
+                <label htmlFor="queryInput">From:</label>
+                <input id="queryInput" value={fromLocation} onChange={e => setFromLocation(e.target.value)} required/>
+                <label htmlFor="queryInput">To:</label>
+                <input id="queryInput" value={toLocation} onChange={e => setToLocation(e.target.value)} required/>
                 <button className="search">Submit</button>
           </form> { showPlaces ? <Places places={places}></Places> : <></>}
           <div>
-              <label className="block">Departure date</label>
-            </div>
+            <Datepicker
+              selected={startDate}
+              onChange={handleStartDate}
+            />
+            <Datepicker
+              selected={endDate}
+              onChange={handleEndDate}
+            />
+          </div>
         </div>
     );
 }
