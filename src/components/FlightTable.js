1b10c6b995354d1a802619fad217abcae9962dcf
cpoptile <77410366+cpoptile@users.noreply.github.com>
2021-03-19 17:58:04 -0500

    Show updated currency in table

--- src/components/FlightTable.js
+++ src/components/FlightTable.js
@@ -1,3 +1,4 @@
+// IMPORTS
 import React, { useState } from 'react';
 
 function FlightTable(props) {
@@ -70,7 +71,7 @@ function FlightTable(props) {
 							<th>{direct}</th>
 							{/* <th>{props.quotes.Carriers[0].Name}</th> */}
 							<th>{departDate.toDateString()}</th>
-							<th>${quote.MinPrice}</th>
+							<th>{props.quotes.Currencies[0].Symbol + " " + quote.MinPrice}</th>
 						</tr>);
 					})}
 				</tbody>
@@ -78,78 +79,6 @@ function FlightTable(props) {
 		</div>
 	);
 
-	// // declaring the default state
-	// state = {
-	// 	currentSort: 'default'
-	// };
-
-
-	// // method called every time the sort button is clicked
-	// // it will change the currentSort value to the next one
-	// onSortChange = () => {
-	// 	const { currentSort } = this.state;
-	// 	let nextSort;
-
-	// 	if (currentSort === 'down') nextSort = 'up';
-	// 	else if (currentSort === 'up') nextSort = 'default';
-	// 	else if (currentSort === 'default') nextSort = 'down';
-
-	// 	this.setState({
-	// 		currentSort: nextSort
-	// 	});
-	// };
-
-
-	// render() {
-	// 	const { data } = this.props;
-	//     console.log(this.props)
-	// 	const { currentSort } = this.state;
-
-	//     const sortTypes = {
-	//         up: {
-	//             class: 'sort-up',
-	//             fn: (a, b) => a.net_worth - b.net_worth
-	//         },
-	//         down: {
-	//             class: 'sort-down',
-	//             fn: (a, b) => b.net_worth - a.net_worth
-	//         },
-	//         default: {
-	//             class: 'sort',
-	//             fn: (a, b) => a
-	//         }
-	//     };
-
-	// 	return (
-	// 		(
-	// 			<table className='text-left'>
-	// 				<thead>
-	// 					<tr>
-	// 						<th>Name</th>
-	// 						<th>
-	// 							Net Worth
-	// 							<button onClick={this.onSortChange}>
-	// 								<i className={`fas fa-${sortTypes[currentSort].class}`} />
-	// 							</button>
-	// 						</th>
-	// 					</tr>
-	// 				</thead>
-	// 				<tbody>
-	// 					{[data].sort(sortTypes[currentSort].fn).map(p => (
-	// 						<tr>
-	//                             {console.log(p)}
-	//                             {console.log(p.Quotes.MinPrice)}
-
-	// 							<td>{p.MinPrice}</td>
-
-	// 							<td>${p.QuoteID}</td>
-	// 						</tr>
-	// 					))}
-	// 				</tbody>
-	// 			</table>
-	// 		)
-	// 	);
-	// }
 }
 
 export default FlightTable;
